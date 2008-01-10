53fb0e3aac0fde43db1e5ade7803643bd842d5a5
lukeredpath <lukeredpath@53438b35-87ee-0310-8825-c8c94c0097b6>
2008-01-10 17:19:24 +0000

    Beanstalk poller encapsulates the looping/polling logic found in most pollers with improved error handling (take 2)
    git-svn-id: http://svn/repos/plugins/beanstalk_messaging@8500 53438b35-87ee-0310-8825-c8c94c0097b6

--- lib/beanstalk_messaging.rb
+++ lib/beanstalk_messaging.rb
@@ -150,4 +150,54 @@ module Beanstalk
       end
   end
   
+  class QueuePoller
+    def initialize(queue_manager, retry_delay = 30, output = STDOUT)
+      @queue_manager = queue_manager
+      @output = output
+      @retry_delay = retry_delay
+    end
+    
+    def puts(object)
+      @output.puts(object)
+    end
+    
+    def load_queue!(queue_name)
+      retry_attempts = 0
+      loop do
+        retry_attempts += 1
+        puts "Attempting to establish connection to queue (attempt #{retry_attempts})"
+        @queue = @queue_manager.reset_queue(queue_name)
+        unless @queue.stale?
+          puts "Connection established."
+          break
+        end
+        sleep @retry_delay
+      end
+    end
+    
+    def poll(queue_name)
+      load_queue!(queue_name)
+      
+      loop do
+        if(pending_messages = @queue.number_of_pending_messages) && pending_messages > 0
+          begin
+            message = @queue.next_message
+            yield message
+            message.delete
+          rescue Beanstalk::UnexpectedResponse => e
+            puts "Unexpected response received from Beanstalk (#{e.message}) Waiting before continuing.".
+            message.release if message
+            sleep @retry_delay
+            next
+          rescue EOFError, Errno::ECONNRESET => e
+            puts "Caught exception: '#{e.message}'. Beanstalk daemon has probably gone away."
+            retry_attempts = 0
+            sleep @retry_delay
+            load_queue!(queue_name)
+          end
+        end
+      end # end loop
+    end # end def   
+  end # end class
+  
 end
