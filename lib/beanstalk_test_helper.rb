266dc625c774c4e1dc93abc149de551c581c6a3d
lukeredpath <lukeredpath@53438b35-87ee-0310-8825-c8c94c0097b6>
2008-10-30 11:03:40 +0000

    The beanstalk test helper should not know anything about QUEUE_MANAGER which is specific to our applications; it should just create its own QueueManager instead.
    git-svn-id: http://svn/repos/plugins/beanstalk_messaging@13852 53438b35-87ee-0310-8825-c8c94c0097b6

--- lib/beanstalk_test_helper.rb
+++ lib/beanstalk_test_helper.rb
@@ -7,7 +7,8 @@ module Beanstalk
       queue_daemon_manager.register_daemon(queue_name.to_s, '0.0.0.0', queue_port)
       queue_daemon_manager.run(queue_name)
       raise "Could not start queue '#{queue_name}'." unless queue_daemon_manager.running?(queue_name)
-      yield QUEUE_MANAGER.queue(queue_name.to_sym) if block_given?
+      queue_manager = Beanstalk::QueueManager.new(@beanstalk_config)
+      yield queue_manager.queue(queue_name.to_sym) if block_given?
     ensure
       if queue_daemon_manager && queue_daemon_manager.running?(queue_name)
         queue_daemon_manager.kill(queue_name.to_s) 
